b74fb5b1f5fc5471583d820d895b46d8b60918f4
Cheng Ma <cm1122@ic.ac.uk>
2024-01-29 00:38:55 +0000

    Implement basic structure of malloc_interceptor

---
+++ malloc_interceptor/malloc_interceptor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <dlfcn.h>
+#include "malloc_interceptor.h"
+
+/* Function pointer to the original malloc function. */
+void (*ori_malloc)(size_t size);
+
+/* Total number of bytes allocated so far. */
+size_t bytes = 0;
+
+/* The intercepted malloc. */
+void *malloc(size_t size) {
+  if (!ori_malloc) {
+    ori_malloc = dlsym(RTLD_NEXT, "malloc");
+  }
+
+  void *fptr = ori_malloc(size);
+  bytes += size;
+
+  fprintf(stderr, "Total bytes allocated so far: %zu\n", bytes);
+  return fptr;
+}
---
+++ malloc_interceptor/malloc_interceptor.h
@@ -0,0 +1,7 @@
+#ifndef MALLOC_INTERCEPTOR_H
+#define MALLOC_INTERCEPTOR_H
+#include <stdio.h>
+
+void *malloc(size_t size);
+
+#endif
